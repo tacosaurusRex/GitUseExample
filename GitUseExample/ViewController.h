556a2f5bb199f2158205e5777b449c69bd01e71e
Bobby <bobby.mahoney@gmail.com>
2015-05-02 12:41:17 -0700

    Initial Commit

---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Robert Mahoney on 5/2/15.
+//  Copyright (c) 2015 RobertMahoney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitUseExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitUseExample
+//
+//  Created by Robert Mahoney on 5/2/15.
+//  Copyright (c) 2015 RobertMahoney. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
